798cfa9614e6307c1d9298c1a9ccfe8bc58010b2
noahk423 <noahk5423@gmail.com>
2017-03-07 23:10:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = joke;
+  botResponse = 'joke'
 
   options = {
     hostname: 'api.groupme.com',
