3fde9b5c2d22429eb7ec1aed7fa8052a95705782
noahk423 <noahk5423@gmail.com>
2017-03-07 22:22:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool$/;
+      botRegex = /^\/lame$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -20,7 +21,7 @@ function respond() {
 
 function postMessage() {
   var botResponse, options, body, botReq;
-
+if (botRegex == /^\/cool$/;
   botResponse = "asdf";
 
   options = {
