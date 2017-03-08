e4fe13bb14605de3a66cc813e969e34c7d9e5d7a
noahk423 <noahk5423@gmail.com>
2017-03-07 22:34:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,40 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool$/;
-      botRegex = /^\/lame$/;
-
+      botRegexL = /^\/lame$/;
+      botRegexHasWorldEnded = /^\/isitover$/;
+      botRegexDildo = /^\/dildo$/;
+    
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+   else if(request.text && botRegexL.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("lame");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+   else if(request.text && botRegexHasWorldEnded.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Not yet in some places.");
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+   else if(request.text && botRegexDildo.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://giphy.com/gifs/UR8GggmzFNIWs");
     this.res.end();
   } else {
     console.log("don't care");
@@ -21,8 +50,8 @@ function respond() {
 
 function postMessage() {
   var botResponse, options, body, botReq;
-if (botRegex == /^\/cool$/;
-  botResponse = "asdf";
+
+  botResponse = response;
 
   options = {
     hostname: 'api.groupme.com',
