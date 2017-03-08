8d4ae5dfb64a8646b6dbeb61e450cc3e6cba88b4
noahk423 <noahk5423@gmail.com>
2017-03-07 23:29:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(cool());
+    postMessage();
     this.res.end();
   } else {
     console.log("don't care");
